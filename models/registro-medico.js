31c577a4b81e102484cf422c669192b3d2ff0436
Karla <karladk038@gmail.com>
2023-10-17 16:36:54 -0600

    Subi cambios

---
+++ models/registro-medico.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const RegistroMedicoSchema = mongoose.Schema({
+
+    // Referencia al la cita agendada
+    cita: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cita',
+        require: true
+    },
+    especialidad: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Especialidades',
+        require: true
+    }, 
+    diagnostico: {
+        type: String,
+        required: true
+    },
+    tratamiento: {
+        type: String,
+        require: true
+    }
+
+})
+
+module.exports = model('registro-medico', RegistroMedicoSchema);
